ab587ed50b46914cd61fa627b8ce76909ce448de
Sharada C <chagisharada29@gmail.com>
2026-04-02 21:07:26 +0530

    Final: Added Render blueprint and build script for Tesseract

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install Tesseract OCR
+apt-get update && apt-get install -y tesseract-ocr
+
+# Install Python dependencies
+pip install -r requirements.txt
